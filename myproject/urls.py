7ba48541df708acba696ea1afa4107cfe5fa49c5
Vaibhav Sahu <vaibhavsahu342@gmail.com>
2022-11-04 22:03:14 +0530

    url change

--- myproject/urls.py
+++ myproject/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
